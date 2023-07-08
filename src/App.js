d5e19968c94b2824ffaa6e7611f4286eb1a9e121
Fclaudiobriito <fclaudio.briito@gmail.com>
2023-07-07 23:39:49 -0300

    Iniciando clone NF em react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+/* eslint-disable import/no-anonymous-default-export */
+import React, {useEffect, useState} from 'react';
+import Tmdb from './Tmdb';
+import MovieRow from './components/movierow';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+export default () => {
+
+  const [movieList, setMovielist] = useState([]);
+
+  useEffect(() => {
+    const loadAll = async () =>{
+
+      // Pegando a lista TOTAL
+      let list = await Tmdb.getHomelist();
+      setMovielist(list);
+      
+    }
+
+    loadAll();
+  }, []);
+
+  return(
+    <div className="page">
+      <section className="lists">
+        {movieList.map((item, key) =>(
+          <MovieRow key={key} title={item.title} items={item.items}/>
+        ))}
+      </section>
     </div>
-  );
+  )
 }
-
-export default App;
